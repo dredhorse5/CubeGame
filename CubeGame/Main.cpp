6e75bd8aa29c1d7a3fb5f651f8190944deb4f6a7
konstantin ttv <titov.k.21@bk.ru>
2020-03-17 15:23:12 +0300

    refresh5

--- CubeGame/Main.cpp
+++ CubeGame/Main.cpp
@@ -18,10 +18,11 @@ void changeSize(int w, int h) {
 
 void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-0.9, -0.9, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glVertex3f(0.5, -0.9, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
 	glEnd();
 
 	glutSwapBuffers();

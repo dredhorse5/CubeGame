ef5d62baa6a1efdd679edab4798b9c08bd9f214f
Титов Дима <dima.titov18@gmail.com>
2020-04-17 11:23:55 +0300

    ва

--- CubeGame/Main.cpp
+++ CubeGame/Main.cpp
@@ -9,13 +9,105 @@
 GLuint dirt[1];
 int View = 90; // angle of view
 int FPS = 60; // 60
+const int quantity_cubes_x = 200;
+const int quantity_cubes_y = 20;
+const int quantity_cubes_z = 200;
 const int width = 1280, height = 720; // размер окна
 float lx = 1.0f, lz = 0.0f, ly = 0.0f; // единичные вектора камеры
 float angleX = 0.0f, angleY = 5.0f; // угол наклона камеры
 int mouseXOld = 1, mouseYOld = 1; // старые коориднаты  мышки
-int size = 1; // размер куба
-int mass[20][20][20];
+float KeyFront = 0, KeySide = 0; // ключ к изменению перемещения вперед/назад
+int cube_size = 2; // размер куба
+int mass[quantity_cubes_x][quantity_cubes_y][quantity_cubes_z];
 
+
+
+class Player {
+public:
+    float PlayerX, PlayerY, PlayerZ;
+    float dx, dy, dz;
+    float dSideX, dSideZ;
+    float dFrontX, dFrontZ;
+    float w, h, d;
+    bool onGround;
+    float speed;
+    float View; // угол обзора
+
+    Player(float x0, float y0, float z0) {
+        PlayerX = x0; PlayerY = y0; PlayerZ = z0;
+        dx = 0; dy = 0; dz = 0;
+        dSideX = 0; dSideZ = 0;
+        dFrontX = 0; dFrontZ = 0;
+        w = 0.5f; h = 1.9f; d = 0.5f; speed = 0.5;
+        onGround = false;
+        View = 90; // угол обзора
+    }
+    bool check(int x, int y, int z) { // функция для проверки столкновения
+        if ((x < 0) or (x > quantity_cubes_x) or
+            (y < 0) or (y > quantity_cubes_y) or
+            (z < 0) or (z > quantity_cubes_z)) return false;
+        return mass[x][y][z];
+
+    }
+    void update(float time) {
+
+        if (PlayerY < 0) { // если игрок провалился под землю, то телепортируем его наверх
+            PlayerY = 20 * cube_size;
+            dy = 0;
+        }
+
+
+        if (KeyFront) { // если активно движение вперед- идем
+            dFrontX = lx * speed * KeyFront * time / 50;
+            dFrontZ = lz * speed * KeyFront * time / 50;
+        }
+        if (KeySide) {// если активно движение вперед- идем
+            dSideX = -lz * speed * KeySide * time / 50;
+            dSideZ = lx * speed * KeySide * time / 50;
+        }
+
+        dy -= 0.12 * (time / 50); // падения, независимое от счетчика FPS
+        onGround = 0;
+
+        dx = dSideX + dFrontX; // получаем общую скорость 
+        PlayerX += dx; // обновляем координаты игрока
+        //std::thread one(Player::*collision, dx, 0, 0);
+        collision(dx, 0, 0); // проверяем столкновение по скорости dx
+        PlayerY += dy * (time / 50);// обновляем координаты игрока
+        collision(0, dy, 0);// проверяем столкновение по скорости dy
+        dz = dSideZ + dFrontZ;// получаем общую скорость 
+        PlayerZ += dz;// обновляем координаты игрока
+        collision(0, 0, dz);// проверяем столкновение по скорости dz
+
+
+        dx = dz = dSideX = dSideZ = dFrontX = dFrontZ = 0; // Обнуляем скорости
+    }
+    
+    void collision(float Dx, float Dy, float Dz) { // функция для взаимодействия игрока с коллизией
+        for (int X = (PlayerX - w) / cube_size; X < (PlayerX + w) / cube_size; X++)
+            for (int Y = (PlayerY - h) / cube_size; Y < (PlayerY + h) / cube_size; Y++)
+                for (int Z = (PlayerZ - d) / cube_size; Z < (PlayerZ + d) / cube_size; Z++)
+                    if (check(X, Y, Z))
+                    {
+                        if (Dx > 0) PlayerX = X * cube_size - w;
+                        if (Dx < 0) PlayerX = X * cube_size + cube_size + w;
+
+                        if (Dy > 0) { PlayerY = Y * cube_size - h; dy = 0; }
+                        if (Dy < 0) { PlayerY = Y * cube_size + cube_size + h; onGround = true; dy = 0; }
+
+                        if (Dz > 0) PlayerZ = Z * cube_size - d;
+                        if (Dz < 0) PlayerZ = Z * cube_size + cube_size + d;
+
+                    }
+    }
+    void jump() { // прыжок
+        if (onGround) {
+            onGround = false;
+            dy = 0.8;
+        }
+    }
+};
+Player steve(quantity_cubes_x / 2 + 2, 60, quantity_cubes_z / 2); // создаем обьект
 void dirtTextures(int W, int H) {
 	unsigned char* topу = SOIL_load_image("textures/dirt.png", &W, &H, 0, SOIL_LOAD_RGB);
 	glGenTextures(1, &dirt[0]);
@@ -43,7 +135,7 @@ void reshape(int w, int h){
 
 void processNormalKeys(unsigned char key, int x, int y) {
     switch(key) {
-    case 27:
+    case 27: // если клавиша esc(27) нажата, то выходим из программы
         exit(0);
     }
 }
@@ -128,29 +220,27 @@ void Draw_cubes() {
         glEnd();
     
 }
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glPushMatrix();
-    gluLookAt(30,         40,     40,       // координаты игрока
-              0 + lx,    5+ly,   0 + lz,  // координаты единичного вектора камеры
-              0.0f,      1.0f,   0.0f   );// координаты нормального вектора камеры. не трогаем. 
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // чистим буфера цвета и глубины
+	glPushMatrix(); // сохраняем систему координат
+    gluLookAt(steve.PlayerX,        steve.PlayerY,      steve.PlayerZ,       // координаты игрока
+              steve.PlayerX + lx,   steve.PlayerY +ly,  steve.PlayerZ + lz,  // координаты единичного вектора камеры
+              0.0f,                 1.0f,               0.0f               );// координаты нормального вектора камеры. не трогаем. 
 
 	glClearColor(0.5, 0.5, 0.5, 1.0); // задаем цвет фона R, G, B, а так же альфа компонента(A), которая задает непрозрачность
 	//=======================================DRAW================================================
 
 
-
-	for (int x = 0; x < 20; x++)
+    // цикл для рисования блоков
+	for (int x = steve.PlayerX/2 - 10; x < steve.PlayerX/2 + 10; x++) // строим блоки  на расстоянии 10 блоков в обе стороны от координаты X игрока
 		for (int y = 0; y < 20; y++)
-			for (int z = 0; z < 20; z++){
+			for (int z = steve.PlayerZ/2 - 10; z < steve.PlayerX/2 + 10; z++){// строим блоки  на расстоянии 10 блоков в обе стороны от координаты Z игрока
 
 
-				if (mass[x][y][z] == 1){
-					glTranslatef(x * size * 2, y * size * 2, z * size * 2);
+				if (mass[x][y][z] == 1){ // если в этом месте есть блок, то рисуем его
+					glTranslatef(x * cube_size , y * cube_size , z * cube_size );
 					Draw_cubes();
-					glTranslatef(-x * size * 2, -y * size * 2, -z * size * 2);
-			
+                    glTranslatef(-x * cube_size, -y * cube_size, -z * cube_size);
 				}
 					
             }
@@ -159,10 +249,10 @@ void display()
 	
 
 
-
+    steve.update(10); // функция обновления обьекта
 	//=======================================DRAW================================================
-	glPopMatrix();
-	glutSwapBuffers();
+	glPopMatrix(); // загружаем систему коориднат
+	glutSwapBuffers(); // меняем буфера
 }
 
 
@@ -187,11 +277,12 @@ int main() {
 
 	dirtTextures(width, height); // загружаем текстуру
 	
-    glutKeyboardFunc(processNormalKeys);
+    glutKeyboardFunc(processNormalKeys); // функция отработки нажатия(без отжатия) клавиш
 	
-	for(int x = 0; x < 20; x++)
-		for(int y = 0; y < 20; y++)
-			for (int z = 0; z < 20; z++) {
+    // цикл для заполнения массива 
+	for(int x = 0; x < quantity_cubes_x; x++)
+		for(int y = 0; y < quantity_cubes_y; y++)
+			for (int z = 0; z < quantity_cubes_z; z++) {
 				if( (y == 5) or (rand() % 10 == 1)) {
 					mass[x][y][z] = 1;
 				}
@@ -200,6 +291,6 @@ int main() {
 				}
 			}
 
-	glutMainLoop();
-	return 0;
+	glutMainLoop(); // говорим, что функция display играется циклично
+	return 0; 
 }
